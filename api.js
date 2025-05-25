7bbb3f21a6b711633437d0042490ed0023e61e57
abdumajid2 <abdumajidt89@gmail.com>
2025-05-25 11:57:28 +0500

    USers

--- api.js
+++ api.js
@@ -1,10 +1,10 @@
 let API = "https://6821ee34b342dce8004c6608.mockapi.io/creat/dataUsers"
-
+import getData from "./dom.js";
 async function get() {
     try {
         let res = await axios.get(API)
         let data = res.data
-
+        getData(data)
     } catch (error) {
         console.log(error);
     }
--- dom.js
+++ dom.js
@@ -1,9 +1,22 @@
-import get from "./api.js";
+// import get from "./api.js";
 let box = document.querySelector(".box");
 
 export default function getData(data) {
     box.innerHTML= ""
     data.forEach((e)=>{
         let conteiner = document.createElement("div");
+        conteiner.classList.add("conteiner");
+
+        let h1Name = document.createElement("h1");
+        h1Name.innerHTML = e.name;
+
+        let pAge = document.createElement("p");
+        pAge.innerHTML = e.age;
+
+        let pStatus = document.createElement("p");
+        pStatus.innerHTML = e.status? "Active" : "Inactive";
+
+        conteiner.append(h1Name, pAge, pStatus);
+        box.append(conteiner);
     })
 }
